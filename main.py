44774754057175a2f565054e8d1eb60d7d7d4c8a
fxcrespo <fxcrespo@al.sp.gov.br>
2018-05-11 17:57:55 -0300

    New client  version

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import bs4
 import json
 from datetime import datetime
+import os
 
 extensions = [
     'fcrespo82.rot',
@@ -10,19 +11,28 @@ extensions = [
     'fcrespo82.markdown-table-formatter'
 ]
 
+e = {}
+if os.path.exists('extensions.json'):
+    with open('extensions.json', 'r+') as json_file:
+        e = json.load(json_file)
 
-print('Extension,Date,Downloads')
 for ext in extensions:
     url = f'https://marketplace.visualstudio.com/items?itemName={ext}'
     response = requests.get(url)
     parser = bs4.BeautifulSoup(response.content, 'html.parser')
-    with open(f'response-{ext}.html', 'w') as f:
-        f.write(parser.prettify())
-    items = parser.findAll('script', attrs= {'class': 'vss-extension'}, recursive=True)
+    items = parser.findAll(
+        'script', attrs={'class': 'vss-extension'}, recursive=True)
     for item in items:
         extension = json.loads(item.text)
         name = extension['displayName']
-        installs = list(filter(lambda x: x['statisticName'] == 'install', extension['statistics']))[0]
+        installs = list(
+            filter(lambda x: x['statisticName'] == 'install', extension['statistics']))[0]
         downloads = int(installs['value'])
-        date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(f'{name},{date},{downloads}')
+        date = datetime.now().strftime('%Y-%m-%d_%H')
+        if not ext in e.keys():
+            e[ext] = {'name': name, 'installs': {}}
+        if not date in e[ext]['installs']:
+            e[ext]['installs'].update({date: downloads})
+
+with open('extensions.json', 'w+') as json_file:
+    json.dump(e, json_file, indent=4)
